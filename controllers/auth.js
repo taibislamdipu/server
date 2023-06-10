d38b877678301b0b5d11f3c15ff06f6fcf0210dd
taibislamdipu <mailtaibislam@gmail.com>
2023-06-11 04:15:23 +0600

    Controller: /login add.

--- controllers/auth.js
+++ controllers/auth.js
@@ -54,3 +54,51 @@ exports.register = async (req, res) => {
     // console.log(err);
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    // 1. destructure email, password from req.body
+    const { email, password } = req.body;
+
+    // 2. all fields require validation
+    if (!email) {
+      return res.json({ error: "Email is required" });
+    }
+
+    if (!password || password.length < 6) {
+      return res.json({ error: "Password must be at least 6 characters long" });
+    }
+
+    // 3. check if email is taken
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.json({ error: "User not taken" });
+    }
+
+    // 4. compare password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({ error: "Invalid email or password" });
+    }
+
+    // 5. create signed jwt
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    // 6. send response
+    res.json({
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (err) {
+    res.status(400).json({ error: err });
+    // console.log(err);
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 // controllers
-const { register } = require("../controllers/auth.js");
+const { register, login } = require("../controllers/auth.js");
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;

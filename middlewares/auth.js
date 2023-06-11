694e00fa7204bdab66992a58810f4416ddc39932
taibislamdipu <mailtaibislam@gmail.com>
2023-06-11 19:57:42 +0600

    Middlewares added.

---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+
+exports.requireSingin = (req, res, next) => {
+  try {
+    const decoded = jwt.verify(
+      req.headers.authorization,
+      process.env.JWT_SECRET
+    );
+
+    req.user = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: error });
+  }
+};
+
+exports.isAdmin = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id); // From where this req came from?
+    if (user.role !== 1) {
+      return res.status(401).send({ message: "Unauthorized" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    return res.status(401).json({ message: error });
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,16 @@ const router = express.Router();
 
 // controllers
 const { register, login } = require("../controllers/auth.js");
+const { requireSingin, isAdmin } = require("../middlewares/auth.js");
 
 router.post("/register", register);
 router.post("/login", login);
+
+router.get("/auth-check", requireSingin, (req, res) => {
+  res.json({ ok: true });
+});
+router.get("/is-admin", requireSingin, isAdmin, (req, res) => {
+  res.json({ ok: true });
+});
 
 module.exports = router;
